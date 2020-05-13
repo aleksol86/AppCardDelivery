6d4ba044e2d9aab556c398660a0f16b7b5f05ed1
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-05-13 11:58:52 +0300

    Обновлён AppCardDeliveryTest

--- src/test/java/ru/netology/domain/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/domain/AppCardDeliveryTest.java
@@ -3,7 +3,8 @@ package ru.netology.domain;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -15,17 +16,15 @@ import static com.codeborne.selenide.Selenide.*;
 public class AppCardDeliveryTest {
 
     @Test
-    void shouldTest1() {
+    void shouldSuccessfulSendForm() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Москва");
         Calendar c = new GregorianCalendar();
         c.add(Calendar.DAY_OF_YEAR, 3);
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
-        String dateForTest = format1.format(c.getTime());
-        for (int i = 0; i < 10; i++) {
-            $("input[placeholder=\"Дата встречи\"]").sendKeys(Keys.BACK_SPACE);
-        }
-        $("input[placeholder=\"Дата встречи\"]").val(dateForTest);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateForTest = LocalDate.now().plusDays(3);
+        $("input[placeholder=\"Дата встречи\"]").sendKeys(Keys.LEFT_CONTROL + "a" + Keys.BACK_SPACE);
+        $("input[placeholder=\"Дата встречи\"]").val(dateForTest.format(formatter));
         $("[data-test-id=name] input").setValue("Медведев Николай");
         $("[data-test-id=phone] input").setValue("+79251234122");
         $("[data-test-id=agreement]").click();
